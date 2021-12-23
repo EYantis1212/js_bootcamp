8dd5c3594424e09b68be4b2e7a18dc68ba55586d
Yantis <yantis1212@outlook.com>
2021-12-23 02:51:57 -0700

    Added Basic Filter for Note Project

--- notes/notes.js
+++ notes/notes.js
@@ -1,1 +1,50 @@
-const notes = ['Note 1', 'Note 2', 'Note 3'];
+// MAIN NOTES ARRAY
+const notes = [
+	{
+		title: 'Career Goal',
+		body: 'I want to become a developer',
+	},
+	{
+		title: 'Habits I want to have',
+		body: 'Learn something, brush teeth, and eat right, everyday',
+	},
+	{
+		title: 'Sealing windows',
+		body: 'Look up prices to order siding to seal windows',
+	},
+	{
+		title: 'Call Dixie',
+		body: 'Call Dixie and let her know the news about VA program',
+	},
+];
+// MAIN FUNCTIONS OBJECTS
+const filters = {
+	searchText: '',
+};
+const renderNotes = function (notes, filters) {
+	const filteredNotes = notes.filter(function (note) {
+		return note.title
+			.toLowerCase()
+			.includes(filters.searchText.toLowerCase());
+	});
+	document.querySelector('#notes').innerHTML = '';
+	filteredNotes.forEach(function (note) {
+		const noteElement = document.createElement('p');
+		noteElement.textContent = note.title;
+		document.querySelector('#notes').appendChild(noteElement);
+	});
+};
+renderNotes(notes, filters);
+// SELECTORS AND LISTENERS
+document.querySelector('#create-note').addEventListener('click', function (e) {
+	e.target.textContent = 'WEEEE!';
+});
+document.querySelector('#remove-all').addEventListener('click', function (e) {
+	document.querySelectorAll('.note').forEach(function (note) {
+		note.remove();
+	});
+});
+document.querySelector('#filter-input').addEventListener('input', function (e) {
+	filters.searchText = e.target.value;
+	renderNotes(notes, filters);
+});
--- todo/todo.js
+++ todo/todo.js
@@ -6,29 +6,64 @@ const todos = [
 	{ text: 'Clean hamster cage', completed: false },
 ];
 
-function removeTodo(arr, text) {
-	const index = arr.findIndex(function (todo, index) {
-		return todo.text.toLowerCase() === text.toLowerCase();
-	});
-	console.log(index);
-	arr.splice(index, 1);
-}
+// SELECTORS
+const paragraphs = document.querySelectorAll('p');
 
-console.log(todos);
-function getTodo(todos) {
-	return todos.filter(function (todo) {
-		return !todo.completed;
-	});
-}
-console.log(getTodo(todos));
+document.querySelector('#add-todo').addEventListener('click', function (e) {
+	const newTodoPara = document.createElement('p');
+	newTodoPara.textContent = newTodo;
+	document.querySelector('#todo').appendChild(newTodoPara);
+});
 
-function sortCompleted(todos) {
-	return todos.sort(function (item1, item2) {
-		if (item1.completed && !item2.completed) {
-			return 1;
-		} else if (!item1.completed && item2.completed) {
-			return -1;
-		} else return 0;
+const newTodo = document
+	.querySelector('#new-todo')
+	.addEventListener('input', function (e) {
+		const newTodoText = e.target.value;
+		return newTodoText;
 	});
-}
-console.log(sortCompleted(todos));
+console.log(newTodo);
+// POPULATE
+
+// ADD TODO FUNCTION
+
+// Summary Line Function
+const summary = todos.filter(function (todo, index) {
+	return !todo.completed;
+});
+const newH2 = document.createElement('h2');
+newH2.textContent = `You have ${summary.length} things left to do`;
+document.querySelector('#todo').appendChild(newH2);
+
+// List Function
+const newParagraph = todos.forEach(function (todo) {
+	const newP = document.createElement('p');
+	newP.textContent = todo.text;
+	document.querySelector('#todo').appendChild(newP);
+});
+
+// function removeTodo(arr, text) {
+// 	const index = arr.findIndex(function (todo, index) {
+// 		return todo.text.toLowerCase() === text.toLowerCase();
+// 	});
+// 	console.log(index);
+// 	arr.splice(index, 1);
+// }
+
+// console.log(todos);
+// function getTodo(todos) {
+// 	return todos.filter(function (todo) {
+// 		return !todo.completed;
+// 	});
+// }
+// console.log(getTodo(todos));
+
+// function sortCompleted(todos) {
+// 	return todos.sort(function (item1, item2) {
+// 		if (item1.completed && !item2.completed) {
+// 			return 1;
+// 		} else if (!item1.completed && item2.completed) {
+// 			return -1;
+// 		} else return 0;
+// 	});
+// }
+// console.log(sortCompleted(todos));
