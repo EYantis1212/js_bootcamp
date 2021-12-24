9679fe012f60c8994a660805d68c964b99b456ee
Yantis <yantis1212@outlook.com>
2021-12-24 14:29:04 -0700

    refactored ToDo App

--- notes/notes.js
+++ notes/notes.js
@@ -17,10 +17,23 @@ const notes = [
 		body: 'Call Dixie and let her know the news about VA program',
 	},
 ];
-// MAIN FUNCTIONS OBJECTS
 const filters = {
 	searchText: '',
 };
+
+// SELECTORS AND LISTENERS
+document.querySelector('#create-note').addEventListener('click', function (e) {
+	e.target.textContent = 'WEEEE!';
+});
+document.querySelector('#filter-input').addEventListener('input', function (e) {
+	filters.searchText = e.target.value;
+	renderNotes(notes, filters);
+});
+document.querySelector('#name-form').addEventListener('submit', function (e) {
+	e.preventDefault();
+	console.log(e.target.elements.firstName.value);
+	e.target.elements.firstName.value = '';
+});
 const renderNotes = function (notes, filters) {
 	const filteredNotes = notes.filter(function (note) {
 		return note.title
@@ -35,16 +48,3 @@ const renderNotes = function (notes, filters) {
 	});
 };
 renderNotes(notes, filters);
-// SELECTORS AND LISTENERS
-document.querySelector('#create-note').addEventListener('click', function (e) {
-	e.target.textContent = 'WEEEE!';
-});
-document.querySelector('#remove-all').addEventListener('click', function (e) {
-	document.querySelectorAll('.note').forEach(function (note) {
-		note.remove();
-	});
-});
-document.querySelector('#filter-input').addEventListener('input', function (e) {
-	filters.searchText = e.target.value;
-	renderNotes(notes, filters);
-});
--- todo/todo.js
+++ todo/todo.js
@@ -1,3 +1,4 @@
+// DATA
 const todos = [
 	{ text: 'Get oil changed on Fiat', completed: false },
 	{ text: 'Get engine light diagnosed', completed: true },
@@ -5,65 +6,47 @@ const todos = [
 	{ text: 'Seal Windows', completed: true },
 	{ text: 'Clean hamster cage', completed: false },
 ];
-
-// SELECTORS
-const paragraphs = document.querySelectorAll('p');
-
-document.querySelector('#add-todo').addEventListener('click', function (e) {
-	const newTodoPara = document.createElement('p');
-	newTodoPara.textContent = newTodo;
-	document.querySelector('#todo').appendChild(newTodoPara);
+const filters = {
+	searchText: '',
+};
+// SELECTORS & LISTENERS
+// Filter Input
+document.querySelector('#filter-text').addEventListener('input', function (e) {
+	filters.searchText = e.target.value;
+	renderTodos(todos, filters);
 });
+// Create ToDo Form
+document.querySelector('#create-todo').addEventListener('submit', function (e) {
+	e.preventDefault();
+	const newTodo = { text: e.target.createTodo.value, completed: false };
+	e.target.createTodo.value = '';
+	todos.push(newTodo);
+	renderTodos(todos, filters);
+});
+// FILTER FUNCTION
 
-const newTodo = document
-	.querySelector('#new-todo')
-	.addEventListener('input', function (e) {
-		const newTodoText = e.target.value;
-		return newTodoText;
+const renderTodos = function (todos, filters) {
+	// Filters Data by Filters Search Text
+	const filteredTodos = todos.filter(function (todo) {
+		return todo.text
+			.toLowerCase()
+			.includes(filters.searchText.toLowerCase());
 	});
-console.log(newTodo);
-// POPULATE
-
-// ADD TODO FUNCTION
-
-// Summary Line Function
-const summary = todos.filter(function (todo, index) {
-	return !todo.completed;
-});
-const newH2 = document.createElement('h2');
-newH2.textContent = `You have ${summary.length} things left to do`;
-document.querySelector('#todo').appendChild(newH2);
-
-// List Function
-const newParagraph = todos.forEach(function (todo) {
-	const newP = document.createElement('p');
-	newP.textContent = todo.text;
-	document.querySelector('#todo').appendChild(newP);
-});
-
-// function removeTodo(arr, text) {
-// 	const index = arr.findIndex(function (todo, index) {
-// 		return todo.text.toLowerCase() === text.toLowerCase();
-// 	});
-// 	console.log(index);
-// 	arr.splice(index, 1);
-// }
-
-// console.log(todos);
-// function getTodo(todos) {
-// 	return todos.filter(function (todo) {
-// 		return !todo.completed;
-// 	});
-// }
-// console.log(getTodo(todos));
-
-// function sortCompleted(todos) {
-// 	return todos.sort(function (item1, item2) {
-// 		if (item1.completed && !item2.completed) {
-// 			return 1;
-// 		} else if (!item1.completed && item2.completed) {
-// 			return -1;
-// 		} else return 0;
-// 	});
-// }
-// console.log(sortCompleted(todos));
+	// Filters completed for summary
+	const incompleteTodos = filteredTodos.filter(function (todo, index) {
+		return !todo.completed;
+	});
+	//Clears DIV of Data on filter
+	document.querySelector('#todo').innerHTML = '';
+	//Creates h2 with Summary Text
+	const summary = document.createElement('h2');
+	summary.textContent = `You have ${incompleteTodos.length} things left to do`;
+	document.querySelector('#todo').appendChild(summary);
+	//Adds filtered Data to HTML Div
+	filteredTodos.forEach(function (todo) {
+		const todoElement = document.createElement('p');
+		todoElement.textContent = todo.text;
+		document.querySelector('#todo').appendChild(todoElement);
+	});
+};
+renderTodos(todos, filters);
